ff1294920d59ed2dbb195ed2fffb49cd8efbf73f
Daniil Bondarenko <60926884+dzzgnr@users.noreply.github.com>
2020-03-21 13:39:22 +0200

    8 Lab Work

--- 1.3.2.2.py
+++ 1.3.2.2.py
@@ -6,20 +6,25 @@ for i in range(15):
 a.sort() # сортировка
 
 def binary_search(a, value):
+    comp_count = 0
     mid = len(a) // 2 # индекс средины списка
     low = 0 # индекс начала списка
     high = len(a) - 1 # индекс конца списка
     # условие поиска :
     # пока центральный элементв не равен искомому и мы не добрались до конца списка
     while a[mid] != value and low <= high:
+        comp_count += 2
         if value > a[mid]: # если искомое значение больше центрального
+            comp_count += 1
             low = mid + 1 # искать будем в правой части списка
         else: # иначе в левой
+            comp_count +=1
             high = mid - 1
             # и меняем индекс центра после перемещения влево
             mid = (low + high) // 2
 
     # если после всех переходов значени не найдено - выводим
+    print("comparsion count =", comp_count)
     if low > high:
         print("no value")
     else: # если найдено - пишем его индекс
