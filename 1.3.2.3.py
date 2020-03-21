c42de8acfe23e34727bdc2c0510352f06652def9
Daniil Bondarenko <60926884+dzzgnr@users.noreply.github.com>
2020-03-21 13:39:01 +0200

    8 Lab Work

--- 1.3.2.3.py
+++ 1.3.2.3.py
@@ -1,1 +1,16 @@
-print('exist') if input("what to find : ") in input("where to find : ") else print('does not exist') # вывод "входит" если то что мы ввели первым находится в том что ввели вторым, вывод "не входит" иначе
+def string_search(x, s):
+    state = 0 #индикатор того, нашли мы строку или нет
+    for i in range(len(s) - len(x) + 1): #будем искать подстроку от начала строки до её конца - длина подстроки
+        j = 0 #итератор подстроки
+        while(j < len(x)): #пока не прошли всю подстроку
+            if (s[i + j] != x[j]): #если один из символов не совпал
+                break #продолжаем искать заново
+            j += 1 #если совпал двигаем на следующий
+
+        if (j == len(x)): #если в итоге количество совпадений = длине подстроки
+            state = 1 #значит мы нашли её в строку
+            print("substring founded at index", i)
+    if state == 0: #если после прохода по строке подстрока не найдена
+        print("substring does not exist")
+
+string_search("qwerty", "fghfdhdfhqwerty")
